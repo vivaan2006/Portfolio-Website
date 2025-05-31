02946f82d8cc0951a553f7a0ba83b91fee0b1adb
Vivaan <vivaanrajesh2006@gmail.com>
2025-05-31 17:07:45 -0600

    Update Contact.tsx

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -12,11 +12,11 @@ const INITIAL_STATE = Object.fromEntries(
   Object.keys(config.contact.form).map((input) => [input, ""])
 );
 
-const emailjsConfig = {
-  serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID,
-  templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-  accessToken: import.meta.env.VITE_EMAILJS_ACCESS_TOKEN,
-};
+// const emailjsConfig = {
+//   serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID,
+//   templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+//   accessToken: import.meta.env.VITE_EMAILJS_ACCESS_TOKEN,
+// };
 
 const Contact = () => {
   const formRef = useRef<React.LegacyRef<HTMLFormElement> | undefined>();
@@ -38,8 +38,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        emailjsConfig.serviceId,
-        emailjsConfig.templateId,
+        'service_pn2q20k',
+        'template_u6oq28e',
         {
           form_name: form.name,
           to_name: config.html.fullName,
@@ -47,7 +47,7 @@ const Contact = () => {
           to_email: config.html.email,
           message: form.message,
         },
-        emailjsConfig.accessToken
+       'E8kC9Ytv4qmxBS7xZ'
       )
       .then(
         () => {
